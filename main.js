c18bec82c6c2f30b1fb8e2bfa1ba5ca7d8d6f23f
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-07-01 22:48:32 +0100

    Olaf png

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ function showFastest(){
         x.style.display = "none";
     }
 }
+function showHamish(){
+    var x = document.getElementById('hamish');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
