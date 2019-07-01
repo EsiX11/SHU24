d1e0d2c61b142b50a6e764d705a14bdda29dec8c
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-07-01 20:08:31 +0100

    JS + CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+function howFastest(){
+    var x = document.getElementById('fastest');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
