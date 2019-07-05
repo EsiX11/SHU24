2e418292c4e6e58ed45b04eb5f7c562e28186f7b
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-07-05 23:28:26 +0100

    Added All

--- main.js
+++ main.js
@@ -25,3 +25,57 @@ function showOlaf(){
         x.style.display = "none";
     }
 }
+function showJoe(){
+    var x = document.getElementById('joe');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function showMattB(){
+    var x = document.getElementById('mattB');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function showMattJ(){
+    var x = document.getElementById('mattJ');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function showEwan(){
+    var x = document.getElementById('ewan');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function showChris(){
+    var x = document.getElementById('chris');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function showJames(){
+    var x = document.getElementById('james');
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
