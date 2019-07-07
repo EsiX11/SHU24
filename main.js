e83fb28b61951a21dfe06ccbde2232a3043ce2f5
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-07-07 23:59:20 +0100

    First update. Waiting for premission

--- main.js
+++ main.js
@@ -1,81 +1,53 @@
 function showFastest(){
     var x = document.getElementById('fastest');
+    var y = document.getElementById('buttons');
+    var z = document.getElementById('average');
     if (x.style.display === "none") {
         x.style.display = "inline-block";
     }
     else {
         x.style.display = "none";
+    }
+    if (y.style.display === "inline-block") {
+        y.style.display = "none";
+    }
+    if (z.style.display === "block") {
+        z.style.display = "none";
     }
 }
-function showHamish(){
-    var x = document.getElementById('hamish');
+
+function showPersonal(){
+    var x = document.getElementById('buttons');
+    var y = document.getElementById('fastest');
+    var z = document.getElementById('average');
     if (x.style.display === "none") {
         x.style.display = "inline-block";
     }
     else {
         x.style.display = "none";
+    }
+    if (y.style.display === "inline-block") {
+        y.style.display = "none";
+    }
+    if (z.style.display === "block") {
+        z.style.display = "none";
     }
 }
-function showOlaf(){
-    var x = document.getElementById('olaf');
+
+function showAverage(){
+    var x = document.getElementById('average');
+    var y = document.getElementById('buttons');
+    var z = document.getElementById('fastest');
     if (x.style.display === "none") {
         x.style.display = "inline-block";
     }
     else {
         x.style.display = "none";
     }
-}
-function showJoe(){
-    var x = document.getElementById('joe');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
+    if (y.style.display === "inline-block") {
+        y.style.display = "none";
     }
-    else {
-        x.style.display = "none";
-    }
-}
-function showMattB(){
-    var x = document.getElementById('mattB');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    }
-    else {
-        x.style.display = "none";
-    }
-}
-function showMattJ(){
-    var x = document.getElementById('mattJ');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    }
-    else {
-        x.style.display = "none";
-    }
-}
-function showEwan(){
-    var x = document.getElementById('ewan');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    }
-    else {
-        x.style.display = "none";
-    }
-}
-function showChris(){
-    var x = document.getElementById('chris');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    }
-    else {
-        x.style.display = "none";
-    }
-}
-function showJames(){
-    var x = document.getElementById('james');
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    }
-    else {
-        x.style.display = "none";
+    if (z.style.display === "inline-block") {
+        z.style.display = "none";
     }
 }
