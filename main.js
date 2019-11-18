75a7bc4f92f8b98d81d4396d2734913fed1be70d
EsiX11 <32106733+EsiX11@users.noreply.github.com>
2019-11-18 18:12:21 +0000

    Whilton (No times)

--- main.js
+++ main.js
@@ -51,3 +51,19 @@ function showAverage(){
         z.style.display = "none";
     }
 }
+var Sum1 = 0;
+var Link = ["images/whilton/1.jpg", "images/whilton/2.jpg", "images/whilton/3.jpg", "images/whilton/4.jpg", "images/whilton/5.jpg", "images/whilton/6.jpg", "images/whilton/7.jpg", "images/whilton/8.jpg", "images/whilton/9.jpg","images/whilton/10.jpg","images/whilton/11.jpg", "images/whilton/12.jpg", "images/whilton/13.jpg", "images/whilton/14.jpg", "images/whilton/15.jpg", "images/whilton/16.jpg", "images/whilton/17.jpg", "images/whilton/18.jpg", "images/whilton/19.jpg" ];
+function backFuction() {
+    Sum1--;
+    if (Sum1 == -1) {
+        Sum1 = 18;
+    } 
+    document.getElementById("whilton").src = Link[Sum1];
+}
+function nextFunction() {
+    Sum1++;
+    if (Sum1 == 19) {
+        Sum1 = 0;
+    }
+    document.getElementById("whilton").src = Link[Sum1];
+}
